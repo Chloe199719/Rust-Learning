474930a0eb168a6b7edb194971f570398c02eb6d
Chloe <chloevision97@gmail.com>
2023-06-05 17:57:55 +0200

    Vectors

---
+++ data_struts/src/main.rs
@@ -0,0 +1,36 @@
+use std::vec::Vec;
+fn main() {
+    // let v: Vec<i32> = Vec::new();
+    // let v = vec![1, 2, 3];
+    let mut v = Vec::new();
+    
+    v.push(5);
+    v.push(6);
+    v.push(7);
+    v.push(8);
+    
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
+
+    let third: Option<&i32> = v.get(2);
+    match third {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
+    
+    // for i in  v {
+    //     println!("{i}");
+    // }
+    
+
+    enum SpreadsheetCell {
+        Int(i32),
+        Float(f64),
+        Text(String),
+    }
+    let row = vec![
+        SpreadsheetCell::Int(3),
+        SpreadsheetCell::Text(String::from("blue")),
+        SpreadsheetCell::Float(10.12),
+    ];
+}
