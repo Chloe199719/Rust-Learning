a21a23977c988339a1a6f2ecbd0cd3155a9b34b1
Chloe Pratas <chloevison97@gmail.com>
2023-06-22 20:47:07 +0200

    comit

---
+++ structs-yt/src/main.rs
@@ -0,0 +1,71 @@
+// struct User {
+//     username: String,
+//     email: String,
+//     sign_in_count: u64,
+//     active: bool,
+// }
+
+// fn main() {
+//     let mut user1 = User {
+//         email: String::from("chloevision97@gmail.com"),
+//         username: String::from("chloevision"),
+//         active: true,
+//         sign_in_count: 1,
+//     };
+//     let name = user1.username;
+//     user1.username = String::from("Chloe ");
+//     let user2 = build_user(String::from("chloevision98@gmail.com"), String::from("Chloe"));
+//     let user3 = User {
+//         email: String::from("email@email.com"),
+//         username: String::from("username"),
+//         ..user2 // struct update syntax,
+//     };
+// }
+
+// fn build_user(email: String, username: String) -> User {
+//     User {
+//         email, // field init shorthand
+//         username,
+//         active: true,
+//         sign_in_count: 1,
+//     }
+// }
+
+// fn tuple_structs() {
+//     struct Color(i32, i32, i32);
+//     struct Point(i32, i32, i32);
+//     let black = Color(0, 0, 0);
+//     let origin = Point(0, 0, 0);
+// }
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+impl Rectangle {
+    fn  area(&self ) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+fn main(){
+    let rect = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 40,
+        height: 50,
+    };
+    let rect1 = Rectangle {
+        width: 20,
+        height: 40,
+    };
+    println!("Can rect hold rect2? {}", rect.can_hold(&rect2));
+    println!("Can rect hold rect1? {}", rect.can_hold(&rect1));
+    println!("rect is {:#?}", rect);
+    println!("The area of the rectangle is {} square pixels.", rect.area());
+}
+
